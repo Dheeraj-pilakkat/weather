7ecd00d0eb04c9b35478610d7735e0413d44d031
Dheeraj <pilakkatdheeraj@gmail.com>
2021-10-13 21:51:17 +0530

    don

--- src/App.jsx
+++ src/App.jsx
@@ -31,32 +31,51 @@ function App() {
     return `${day} ${date} ${month} ${year}`
   }
   return (
-    <div className={(typeof weather.main !="undefined")?((weather.main.temp >16)?'app warm':'app'):'app'}>
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
           <input
-           type="text"
-           className="search-bar"
-           placeholder="search..." 
-           onChange={e=>setQuery(e.target.value)}
-           value={query}
-           onKeyPress={search}   
+            type="text"
+            className="search-bar"
+            placeholder="search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
-        {(typeof weather.main != "undefined") ? (
-        <><div className="location-box">
-            <div className="location">{weather.name},{weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div><div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°c</div>
-              <div className="weather">
-                {weather.weather[0].main}
+        {typeof weather.main != "undefined" ? (
+          <>
+            <div className="location-box">
+              <div className="location">
+                {weather.name},{weather.sys.country}
               </div>
-            </div></>
-        ): ('')}
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°c <br />{" "}
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
       </main>
+      <div className="made">
+        Made by  <strong>Dheeraj.p</strong><br />
+        ©   2021 
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
